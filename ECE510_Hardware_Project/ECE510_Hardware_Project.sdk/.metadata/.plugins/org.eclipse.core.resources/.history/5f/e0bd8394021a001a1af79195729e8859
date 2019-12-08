571c252c985561348ef53e55acccea9d6f4954d4
schimpfen <schimpfen@github.com>
2019-12-08 17:44:47 -0500

    SDK UART and ALU math

---
+++ ECE510_Hardware_Project/ECE510_Hardware_Project.sdk/.metadata/.plugins/org.eclipse.core.resources/.history/5f/e0bd8394021a001a1af79195729e8859
@@ -0,0 +1,8 @@
+/*
+ * Main.c
+ *
+ *  Created on: Dec 8, 2019
+ *      Author: natha
+ */
+
+
--- ECE510_Hardware_Project/ECE510_Hardware_Project.sdk/ALU_Firmware/Debug/src/Main.d
+++ ECE510_Hardware_Project/ECE510_Hardware_Project.sdk/ALU_Firmware/Debug/src/Main.d
@@ -1,1 +1,43 @@
-src/Main.o src/Main.o: ../src/Main.c
+src/Main.o src/Main.o: ../src/Main.c \
+ ../../ALU_Firmware_bsp/microblaze_0/include/xparameters.h \
+ ../../ALU_Firmware_bsp/microblaze_0/include/xbasic_types.h \
+ ../../ALU_Firmware_bsp/microblaze_0/include/xgpio.h \
+ ../../ALU_Firmware_bsp/microblaze_0/include/xil_types.h \
+ ../../ALU_Firmware_bsp/microblaze_0/include/xil_assert.h \
+ ../../ALU_Firmware_bsp/microblaze_0/include/xstatus.h \
+ ../../ALU_Firmware_bsp/microblaze_0/include/xgpio_l.h \
+ ../../ALU_Firmware_bsp/microblaze_0/include/xil_io.h \
+ ../../ALU_Firmware_bsp/microblaze_0/include/xil_printf.h \
+ ../../ALU_Firmware_bsp/microblaze_0/include/xparameters.h \
+ ../../ALU_Firmware_bsp/microblaze_0/include/bspconfig.h \
+ ../../ALU_Firmware_bsp/microblaze_0/include/mb_interface.h \
+ ../../ALU_Firmware_bsp/microblaze_0/include/xil_exception.h \
+ ../../ALU_Firmware_bsp/microblaze_0/include/xstatus.h
+
+../../ALU_Firmware_bsp/microblaze_0/include/xparameters.h:
+
+../../ALU_Firmware_bsp/microblaze_0/include/xbasic_types.h:
+
+../../ALU_Firmware_bsp/microblaze_0/include/xgpio.h:
+
+../../ALU_Firmware_bsp/microblaze_0/include/xil_types.h:
+
+../../ALU_Firmware_bsp/microblaze_0/include/xil_assert.h:
+
+../../ALU_Firmware_bsp/microblaze_0/include/xstatus.h:
+
+../../ALU_Firmware_bsp/microblaze_0/include/xgpio_l.h:
+
+../../ALU_Firmware_bsp/microblaze_0/include/xil_io.h:
+
+../../ALU_Firmware_bsp/microblaze_0/include/xil_printf.h:
+
+../../ALU_Firmware_bsp/microblaze_0/include/xparameters.h:
+
+../../ALU_Firmware_bsp/microblaze_0/include/bspconfig.h:
+
+../../ALU_Firmware_bsp/microblaze_0/include/mb_interface.h:
+
+../../ALU_Firmware_bsp/microblaze_0/include/xil_exception.h:
+
+../../ALU_Firmware_bsp/microblaze_0/include/xstatus.h:
